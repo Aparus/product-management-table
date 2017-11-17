76318b072e98d16d74ca7f5d86f2d98cb8e7cde3
Rustam Apaev <rapaev@abisoft.biz>
2017-11-17 20:48:55 +0300

    added 3 simple tests for 3 type of actions

--- src/product/reducers.spec.js
+++ src/product/reducers.spec.js
@@ -1,5 +1,5 @@
 import deepFreeze from 'deep-freeze';
-import { product } from './reducers';
+import { products, visibilityFilter } from './reducers';
 
 test('test add product', () => {
   const stateBefore = [];
@@ -7,20 +7,65 @@ test('test add product', () => {
   const action = {
     type: 'ADD_PRODUCT',
     payload: {
-      SKUNumber: 'A1B1C1',
-      ProductName: 'SIP Connectivity',
+      SKUNumber: '12345678',
+      name: 'SIP Connectivity',
     },
   };
 
   const stateAfter = [
     {
-      SKUNumber: 'A1B1C1',
-      ProductName: 'SIP Connectivity',
+      SKUNumber: '12345678',
+      name: 'SIP Connectivity',
+      selected: false,
     },
   ];
 
   deepFreeze(stateBefore);
   deepFreeze(action);
 
-  expect(product(stateBefore, action)).toEqual(stateAfter);
+  expect(products(stateBefore, action)).toEqual(stateAfter);
+});
+
+test('test toggle product selection', () => {
+  const stateBefore = [
+    {
+      SKUNumber: '12345678',
+      name: 'SIP Connectivity',
+      selected: false,
+    },
+  ];
+
+  const action = {
+    type: 'TOGGLE_PRODUCT',
+    SKUNumber: '12345678',
+  };
+
+  const stateAfter = [
+    {
+      SKUNumber: '12345678',
+      name: 'SIP Connectivity',
+      selected: true,
+    },
+  ];
+
+  deepFreeze(stateBefore);
+  deepFreeze(action);
+
+  expect(products(stateBefore, action)).toEqual(stateAfter);
+});
+
+test('test Visibility filter change', () => {
+  const stateBefore = 'SHOW_ALL';
+
+  const action = {
+    type: 'SET_VISIBILITY_FILTER',
+    filter: 'SHOW_SELECTED',
+  };
+
+  const stateAfter = 'SHOW_SELECTED';
+
+  deepFreeze(stateBefore);
+  deepFreeze(action);
+
+  expect(visibilityFilter(stateBefore, action)).toEqual(stateAfter);
 });
